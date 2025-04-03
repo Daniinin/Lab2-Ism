c0785790e08e0bbfba14894f7b15943c214e4bef
Daniinin <danielnicolae.pinca@ulbsibiu.ro>
2025-04-03 10:19:27 +0300

    Merge

--- src/Lab4/Declaratii.java
+++ src/Lab4/Declaratii.java
@@ -1,54 +1,58 @@
+package Lab4;
+
 import java.util.*;
 
-public static void main(String[] args) {
-    Random rand = new Random();
-    List<Integer> x = new ArrayList<>();
-    List<Integer> y = new ArrayList<>();
-    List<Integer> xPlusY = new ArrayList<>(); // a
-    Set<Integer> zSet = new TreeSet<>(); // b
-    List<Integer> xMinusY = new ArrayList<>(); // c
-    int p = 4;
-    List<Integer> xPlusYLimitedByP = new ArrayList<>(); // d
+public class Declaratii {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        List<Integer> x = new ArrayList<>();
+        List<Integer> y = new ArrayList<>();
+        List<Integer> xPlusY = new ArrayList<>(); // a
+        Set<Integer> zSet = new TreeSet<>(); // b
+        List<Integer> xMinusY = new ArrayList<>(); // c
+        int p = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList<>(); // d
 
-    // Generare valori aleatorii pentru x și y
-    for (int i = 0; i < 5; i++) x.add(Integer.valueOf(rand.nextInt(11)));
-    for (int i = 0; i < 5; i++) y.add(Integer.valueOf(rand.nextInt(11)));
+        // Generare valori aleatorii pentru x și y
+        for (int i = 0; i < 5; i++) x.add(Integer.valueOf(rand.nextInt(11)));
+        for (int i = 0; i < 5; i++) y.add(Integer.valueOf(rand.nextInt(11)));
 
-    // Sortare liste x și y
-    Collections.sort(x);
-    Collections.sort(y);
+        // Sortare liste x și y
+        Collections.sort(x);
+        Collections.sort(y);
 
-    // a) xPlusY conține toate elementele din x și y
-    xPlusY.addAll(x);
-    xPlusY.addAll(y);
-    Collections.sort(xPlusY);
+        // a) xPlusY conține toate elementele din x și y
+        xPlusY.addAll(x);
+        xPlusY.addAll(y);
+        Collections.sort(xPlusY);
 
-    // b) zSet conține valorile comune din x și y, fără duplicate
-    for (Integer val : x) {
-        if (y.contains(val)) {
-            zSet.add(val);
+        // b) zSet conține valorile comune din x și y, fără duplicate
+        for (Integer val : x) {
+            if (y.contains(val)) {
+                zSet.add(val);
+            }
         }
-    }
-    // c) xMinusY conține elementele din x care nu sunt în y
-    for (Integer val : x) {
-        if (!y.contains(val)) {
-            xMinusY.add(val);
+        // c) xMinusY conține elementele din x care nu sunt în y
+        for (Integer val : x) {
+            if (!y.contains(val)) {
+                xMinusY.add(val);
+            }
         }
-    }
-    Collections.sort(xMinusY);
+        Collections.sort(xMinusY);
 
-    // d) xPlusYLimitedByP conține elementele din x și y ce nu depășesc p
-    for (Integer val : xPlusY) {
-        if (val <= p) {
-            xPlusYLimitedByP.add(val);
+        // d) xPlusYLimitedByP conține elementele din x și y ce nu depășesc p
+        for (Integer val : xPlusY) {
+            if (val <= p) {
+                xPlusYLimitedByP.add(val);
+            }
         }
-    }
 
-    // Afișare rezultate
-    System.out.println("Lista x: " + x);
-    System.out.println("Lista y: " + y);
-    System.out.println("Lista xPlusY: " + xPlusY);
-    System.out.println("Set zSet (valori comune): " + zSet);
-    System.out.println("Lista xMinusY: " + xMinusY);
-    System.out.println("Lista xPlusYLimitedByP: " + xPlusYLimitedByP);
+        // Afișare rezultate
+        System.out.println("Lista x: " + x);
+        System.out.println("Lista y: " + y);
+        System.out.println("Lista xPlusY: " + xPlusY);
+        System.out.println("Set zSet (valori comune): " + zSet);
+        System.out.println("Lista xMinusY: " + xMinusY);
+        System.out.println("Lista xPlusYLimitedByP: " + xPlusYLimitedByP);
+    }
 }
---
+++ src/ro/ulbs/paradigme/lab3/util/Main.java
@@ -0,0 +1,13 @@
+package ro.ulbs.paradigme.lab3.util;
+public class Main {
+    public static void main(String[] args) {
+        ro.ulbs.paradigme.lab3.util.PasswordMaker pm = new ro.ulbs.paradigme.lab3.util.PasswordMaker("Madalin");
+        System.out.println("Generated Password: " + pm.getPassword());
+    }
+}
+
+
+
+
+
+
