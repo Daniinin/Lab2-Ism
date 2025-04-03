c99158c85d113b630176fc17404c17e40a52471d
Daniinin <danielnicolae.pinca@ulbsibiu.ro>
2025-04-03 10:24:33 +0300

    Merge

---
+++ src/Lab6/NewIntCalculator.java
@@ -0,0 +1,29 @@
+package Lab6;
+
+class NewIntCalculator extends ACalculator<Integer> {
+
+    public NewIntCalculator(Integer initialValue) {
+        super(initialValue);
+    }
+
+    @Override
+    public NewIntCalculator init(Integer value) {
+        this.state = value;
+        return this;
+    }
+
+    public NewIntCalculator add(int value) {
+        this.state += value;
+        return this;
+    }
+
+    public NewIntCalculator subtract(int value) {
+        this.state -= value;
+        return this;
+    }
+
+    public NewIntCalculator multiply(int value) {
+        this.state *= value;
+        return this;
+    }
+}
